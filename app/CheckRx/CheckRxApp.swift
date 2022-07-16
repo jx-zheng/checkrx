38bed1615d45e679a4b51982889651ee4621ed68
Kevin Zheng <jxzheng@uwaterloo.ca>
2022-07-16 06:27:55 -0400

    start base app, backend

---
+++ app/CheckRx/CheckRxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckRxApp.swift
+//  CheckRx
+//
+//  Created by Kevin Zheng on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct CheckRxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
